4ac377ad70dfe607ab8fb7766d0a6c7acb12729d
ho <refout28@gmail.com>
2021-05-26 21:06:47 +0900

    commit_1

---
+++ algorithm/main.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def result(self):
+#         return 1
+
+# if __name__ == "__main__":
+#     s = Solution()
+#     print(s.result())
+
+def get_natural_number():
+    n = 0
+    while True:
+        n += 1
+        yield n
+
+g = get_natural_number()
+for _ in range(0,100):
+    print(next(g))
+    
+print("git test____!")
