759079eda46814c9737abbf7583dceb0442e56c9
ho <refout28@gmail.com>
2021-06-18 13:04:09 +0900

    commit_test

--- algorithm/main.py
+++ algorithm/main.py
@@ -6,17 +6,30 @@
 #     s = Solution()
 #     print(s.result())
 
-def get_natural_number():
-    n = 0
-    while True:
-        n += 1
-        yield n
+# def get_natural_number():
+#     n = 0
+#     while True:
+#         n += 1
+#         yield n
 
-g = get_natural_number()
-for _ in range(0,100):
-    print(next(g))
+# g = get_natural_number()
+# for _ in range(0,100):
+#     print(next(g))
     
 
-print("test git!!!_210526")
-print("1")
-print("test_0529")
+# print("test git!!!_210526")
+# print("1")
+# print("test_0529")
+
+
+num = 987
+
+mod_num = list(map(int, str(num)))
+# print([int(i) for i in str(num)])
+sum = 0
+
+for i in mod_num:
+    sum += i
+
+
+print(sum)
